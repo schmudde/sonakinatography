5ba58f9650309c23bddf5cad5f58d463f0c03a37
schmudde <d@schmud.de>
2019-07-29 17:19:52 -0400

    Animation is working.

--- sonakinatography.py
+++ sonakinatography.py
@@ -46,11 +46,14 @@ class Shape:
 
   def paint_square_to_group(self, canvas, group):
     group.add(canvas.rect((self.x, self.y), (self.square_size, self.square_size), fill=self.color_grid[self.color]))
-    # return canvas.rect((self.x, self.y), (self.square_size, self.square_size), fill=self.color_grid[self.color])
 
   def paint_line(self, canvas):
     self.x = self.x + (self.square_size / 2) - 2
     canvas.add(canvas.rect((self.x, self.y), (3, self.square_size), fill=self.color_grid[self.color]))
+
+  def paint_line_to_group(self, canvas, group):
+    self.x = self.x + (self.square_size / 2) - 2
+    group.add(canvas.rect((self.x, self.y), (3, self.square_size), fill=self.color_grid[self.color]))
 
 ##### Simple Example
 
@@ -86,26 +89,28 @@ def reset_instrument(instrument):
   instrument['color_grid_val'] = instrument['advancer'](instrument['color_grid_val'])
   instrument['countdown'] = color_grid[instrument['color_grid_val']][instrument['instrument']-1]
 
-def build_row(canvas, grid_objects, beat, max_beat):
+def build_row(canvas, shape_group, grid_objects, beat, max_beat):
   for instrument in grid_objects:
     instrument['countdown'] -= 1
     instrument['color'] = color_grid[instrument['color_grid_val']][instrument['instrument']-1]
     shape = Shape(instrument, beat, max_beat)
     if instrument['countdown'] == 0:
-      shape.paint_square(canvas)
+      shape.paint_square_to_group(canvas, shape_group)
+      # shape.paint_square(canvas)
       reset_instrument(instrument)
     else:
-      shape.paint_line(canvas)
+      shape.paint_line_to_group(canvas, shape_group)
+#      shape.paint_line(canvas)
 
-def build_columns(canvas, grid_objects, max_beat):
+def build_columns(canvas, shape_group, grid_objects, max_beat):
   beat = 0
   while beat < max_beat:
     beat += 1
-    build_row(canvas, grid_objects, beat, max_beat)
+    build_row(canvas, shape_group, grid_objects, beat, max_beat)
 
 def build_matrix(max_beat):
-  dwg = svgwrite.Drawing('sonakinatography.svg', profile='tiny')
-
+  canvas = svgwrite.Drawing('sonakinatography.svg', profile='tiny')
+  group = canvas.add(canvas.g())
 
   # TODO: Update reset_instrument and create a software instantiation so that instrument x can start at any number.
   grid_objects = [{'color': 1, 'countdown': 1, 'instrument': 1, 'advancer': lambda y: y-1, 'color_grid_val': 7},
@@ -116,8 +121,12 @@ def build_matrix(max_beat):
                   {'color': 6, 'countdown': 6, 'instrument': 6, 'advancer': lambda y: y-1, 'color_grid_val': 7},
                   {'color': 7, 'countdown': 7, 'instrument': 7, 'advancer': lambda y: y-1, 'color_grid_val': 7},
                   {'color': 8, 'countdown': 8, 'instrument': 8, 'advancer': lambda y: y-1, 'color_grid_val': 7}]
-  build_columns(dwg, grid_objects, max_beat)
-  return dwg
+  build_columns(canvas, group, grid_objects, max_beat)
+
+  # TODO: This currently animates the sonakinatography in an ugly way. Refactor and polish animation.
+
+  group.add(canvas.animateTransform("translate","transform",id="polygon",from_="0,0", to="0,450",dur="4s",begin="0s",repeatCount="indefinite"))
+  return canvas
 
 def run_simple():
   draw = simple_shapes({'color': 2, 'instrument': 1}, {'color': 2, 'instrument': 3})
@@ -129,13 +138,15 @@ def run_full(no_of_beats):
 
 # (execfile('sonakinatography.py'))
 
-def run_animate(no_of_beats):
+def run_animate():
+
+    # Simple animation test
 
     canvas = svgwrite.Drawing('results/animation-test.svg',profile='tiny',size=(600,400))
 
     def add_instrument_to_group(canvas, shape_group, instrument):
       shape = Shape(instrument, 1, 1)
-      shape.paint_square_to_group(canvas, shape_group)
+      shape.paint_line_to_group(canvas, shape_group)
 
     group = canvas.add(canvas.g())
 
